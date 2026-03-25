d1aef88da575941aadfd6b36c95ac7fdf5d1251f
invoices-nycitymarshal <invoices@nycitymarshal.com>
2026-03-25 11:00:07 -0400

    Created tenet info.js

--- dailyAPS/draftAPS.js
+++ dailyAPS/draftAPS.js
@@ -1,1 +1,12 @@
-// hello world
+// from clean csv import clean csv
+
+function sendCPREmail() {
+
+    const uneditedCSV = ""
+    const editedCSV = cleanCSV(masterCSV)
+
+    // instantiate empty array with information
+    // pass through 
+
+
+}
