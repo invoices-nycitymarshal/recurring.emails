0677e42d9f6de9e71eccb113032bbc0045f9ce4c
invoices-nycitymarshal <invoices@nycitymarshal.com>
2026-03-25 11:10:16 -0400

    Listed relevant CSV columns

--- dailyAPS/cleanCSV.js
+++ dailyAPS/cleanCSV.js
@@ -1,1 +1,19 @@
-// hello world
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const relevantColumns = [
+  'court_index',
+  'executed_by',
+  'tenet_name',
+  'eviction address',
+  'eviction_apt_num',
+  'eviction_city',
+  'eviction_state',
+  'eviction_zip',
+  'psa_ctrl_no',
+  'aps_involvement_flag'
+];
+    
+function extractColumns(filePath) {
+
+}
